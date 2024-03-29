6edf9bbe2b23158d2bcc462c3c9aff866ba85410
tjatn304905 <tjatn304905@gmail.com>
2024-03-29 13:51:06 +0900

    inital commit

---
+++ src/main/java/com/example/springbootdockerecs/SpringBootDockerEcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerEcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdockerecs/SpringBootDockerEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
